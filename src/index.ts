8729a24d2f44fee825a208bb3804e6cfab8a7591
Collins Self <collins@mini.local>
2023-07-05 19:51:34 -0400

    init WebGPU resources

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import "./styles.css"
 
-async function testWebGPU() {
-    const canvas = document.querySelector('canvas');
-    const adapter = await navigator.gpu.requestAdapter();
-    if (!adapter) {
-        throw new Error("WebGPU was not able to load in your browser.");
-    }
-    const device = await adapter.requestDevice();
-    const queue = device.queue;
+const canvas = document.querySelector('canvas');
+
+// initialize GPU adapter needed for device access
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+    throw new Error("WebGPU was not able to load in your browser.");
 }
-testWebGPU();
+
+// device used for interfacing with the GPU
+const device = await adapter.requestDevice();
+const queue = device.queue;
+
+// configure the Canvas
+// todo:  come back and learn more about the config options
+const context = canvas.getContext("webgpu");
+context.configure({
+    device,
+    format: navigator.gpu.getPreferredCanvasFormat(),
+    alphaMode: 'opaque',
+});
+
+const texture = context.getCurrentTexture();
+const textureView = texture.createView();
